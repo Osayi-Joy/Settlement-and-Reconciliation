4cfa32cdac8634167ff331714c5fb692675d5599
Joy Osayi <osayijoy16@gmail.com>
2024-01-15 18:54:19 +0100

    Reconciliation

---
+++ src/main/java/com/osayijoy/settlement_reconciliation/SettlementReconciliationApplication.java
@@ -0,0 +1,13 @@
+package com.example.settlement_reconciliation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SettlementReconciliationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SettlementReconciliationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/settlement_reconciliation/SettlementReconciliationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.settlement_reconciliation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SettlementReconciliationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
